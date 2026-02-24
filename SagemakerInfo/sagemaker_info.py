2bab70418d951bb781adb58504c5271662b21abf
MayurNandanwar <88454062+MayurNandanwar@users.noreply.github.com>
2026-02-24 17:21:24 +0530

    Add files via upload

---
+++ SagemakerInfo/sagemaker_info.py
@@ -0,0 +1,7 @@
+Domain: Domain is an environment for your team to access SageMaker resources.Domain consist of list of 
+users and users within domain can share notebookfiles and other artifects with each other.oct
+One account can have one or multiple domain
+
+Code Editor: it is abasically entire IDE in the cloud.
+Studio classic is kind a notebook
+Rstudio works with programming language R 
