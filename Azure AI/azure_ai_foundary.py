a89e63e9ee4a48074e04e98347cad05ed9029ce6
MayurNandanwar <88454062+MayurNandanwar@users.noreply.github.com>
2026-02-24 17:18:08 +0530

    aa
    aa

---
+++ Azure AI/azure_ai_foundary.py
@@ -0,0 +1,7 @@
+Azure ai foundary sdk  is comprehensive  toolchain designed to simplify the development of ai applications on azure
+it enables to developers to:
+1) access popular models from various model providers through single interface
+2)combine togather models, data and ai services to build ai-powered applications
+3) Evaluate, debug and improve application quality and safety across development,testing and production environment.
+
+
